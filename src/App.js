58147f556ced9e905419f17f9bdcd5a2f83f2761
LinKaiRu <10746041@ntub.edu.tw>
2022-08-14 15:48:27 +0800

    sidebar

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Sidebar from './components/Sidebar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Overview from './pages/Overview'
+import {Reports,ReportsOne,ReportsTwo,ReportsThree} from './pages/Reports'
+import Team from './pages/Team';
+function App() {
+  return (
+    <Router>
+      <Sidebar />
+      <Routes>
+        <Route path="/overview" exact element={<Overview/>} />
+        <Route path="/reports" exact element={<Reports/>} />
+        <Route path="/reports/reports1" exact element={<ReportsOne/>} />
+        <Route path="/reports/reports2" exact element={<ReportsTwo/>} />
+        <Route path="/reports/reports3" exact element={<ReportsThree/>} />
+        <Route path="/team" exact element={<Team/>} />
+
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
